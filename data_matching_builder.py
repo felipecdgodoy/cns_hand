7332687a500cc0fe6fd4c4ed57e6932f5af488d6
Felipe Godoy <54994053+felipecdgodoy@users.noreply.github.com>
2024-04-16 03:46:08 -0700

    switched default to high-def imgs and custom dimensions

--- data_matching_builder.py
+++ data_matching_builder.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import pickle as pk
 import nibabel as nib
+from tqdm import tqdm
 
 # set deterministic behavior
 np.random.seed(0)
 random.seed(0)
 
-import warnings
-warnings.filterwarnings('ignore')
+COMMON_SIZE = 96 # dim for image 3D-volume (DxDxD)
+SAVE_PATH = '/scratch/groups/kpohl/matched_patients.pickle'
 
 def get_CE_label(numeric_label):
     """ map single digit label (0, 1, 2, 3) -> 2-bit binary label (00, 01, 10, 11)"""
@@ -20,44 +21,40 @@ with open('new_ucsf.pickle', 'rb') as f:
     ucsf = pk.load(f)
 
 ucsf = pd.DataFrame(ucsf, columns=['filename', 'label', 'dataset', 'all_dataset', 'id', 'age', 'gender', 'npz'])
+### Update to high-definition images
+ucsf['filename'] = ucsf['filename'].apply(lambda path : path.replace('img_64_', 'img_orig_'))
+###
 ucsf['id'] = ucsf['id'].astype(int)
 ucsf.loc[np.abs(ucsf['npz']) > 3.5, 'npz'] = np.nan
 ucsf['label'] = ucsf['label'].apply(lambda x : get_CE_label(x))
 ucsf = ucsf.drop_duplicates(subset=['id'])
 ucsf = ucsf[['id', 'age', 'gender', 'npz', 'label', 'filename']]
 
-patch_x, patch_y, patch_z = 64, 64, 64
+paths = ucsf['filename']
+n = len(ucsf)
+x, y, z = nib.load(paths[0]).get_fdata().shape
+ucsf['image'] = [np.zeros((x, y, z))] * n
 
-images = list()
+patch_x = (x - COMMON_SIZE) // 2
+patch_y = (y - COMMON_SIZE) // 2
+patch_z = (z - COMMON_SIZE) // 2
 
-for filename in ucsf['filename']:
+for i in tqdm(range(len(paths)), desc='processing images'):
+    filename = paths[i]
     img = nib.load(filename)
-    img_data = img.get_fdata()
-    img_data = img_data[0:patch_x, 0:patch_y, 0:patch_z]
+    img_data = img.get_fdata() # original size: 138 x 176 x 138
+    img_data = img_data[patch_x : -patch_x, patch_y : -patch_y, patch_z : -patch_z]
     img_data = (img_data - np.mean(img_data)) / np.std(img_data)
-    images.append(img_data)
+    ucsf.loc[ucsf['filename'] == filename, 'image'] = [img_data]
 
-ucsf['image'] = images
+print(f'Matched Patients: {len(ucsf)}')
 
-augm_hiv_block_1 = ucsf[ucsf['label'].astype(str) == '[0 1]']
-augm_hiv_block_2 = ucsf[ucsf['label'].astype(str) == '[0 1]']
-augm_hiv_block_3 = ucsf[ucsf['label'].astype(str) == '[0 1]']
-augm_hiv_block_4 = ucsf[ucsf['label'].astype(str) == '[0 1]']
-
-augm_hiv_block_1['image'] = augm_hiv_block_1['image'].apply(lambda img : img + np.random.normal(loc=0, scale=0.02, size=(patch_x, patch_y, patch_z)))
-augm_hiv_block_2['image'] = augm_hiv_block_2['image'].apply(lambda img : img + np.random.normal(loc=0, scale=0.05, size=(patch_x, patch_y, patch_z)))
-augm_hiv_block_3['image'] = augm_hiv_block_3['image'].apply(lambda img : img + np.random.normal(loc=0, scale=0.10, size=(patch_x, patch_y, patch_z)))
-augm_hiv_block_4['image'] = augm_hiv_block_3['image'].apply(lambda img : img + np.random.normal(loc=0, scale=0.12, size=(patch_x, patch_y, patch_z)))
-
-ucsf = pd.concat((augm_hiv_block_1, augm_hiv_block_2, augm_hiv_block_3, augm_hiv_block_4, ucsf))
-
-print(f'Matched Patients: {len(images)}')
-print(f'Generated Samples: {len(ucsf)}')
+with open(SAVE_PATH, 'wb') as handle:
+    pk.dump(ucsf, handle)
 
 print(f"CTRL: {len(ucsf[ucsf['label'].astype(str) == '[0 0]'])}")
 print(f"MCI: {len(ucsf[ucsf['label'].astype(str) == '[1 0]'])}")
 print(f"HIV: {len(ucsf[ucsf['label'].astype(str) == '[0 1]'])}")
 print(f"HAND: {len(ucsf[ucsf['label'].astype(str) == '[1 1]'])}")
 
-with open('matched_patients.pickle', 'wb') as handle:
-    pk.dump(ucsf, handle)
+print(f"Successfully Saved: {SAVE_PATH}")
