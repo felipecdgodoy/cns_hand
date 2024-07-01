bac5f2cc7fbdacdbc664f493638f55fcd8149274
Felipe Godoy <54994053+felipecdgodoy@users.noreply.github.com>
2024-07-01 16:32:58 -0700

    codestyle
    adjusted the codestyle to prepare for submission

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@ import csv
 
 class CSVLogger():
     
-    def __init__(self,  args,fieldnames, filename='log.csv'):
+    def __init__(self, args, fieldnames, filename='log.csv'):
         self.filename = filename
         self.csv_file = open(filename, 'w')
         writer = csv.writer(self.csv_file)
@@ -17,20 +17,20 @@ class CSVLogger():
         self.writer.writerow(row)
         self.csv_file.flush()
 
-    def final(self,best_accuracy_list,final_accuracy_list,best_epoch_list,ave_valid_acc_50_list,acc_each_class_list,acc_each_dataset_list = None):
+    def final(self, best_accuracy_list, final_accuracy_list, best_epoch_list, avg_valid_acc_50_list, acc_each_class_list, acc_each_dataset_list=None):
         writer = csv.writer(self.csv_file)
-        best_accuracy_list.insert(0,'best acc')
-        final_accuracy_list.insert(0,'final acc')
-        best_epoch_list.insert(0,'best epoch')
-        ave_valid_acc_50_list.insert(0,'ave_valid_acc_50')
-        acc_each_class_list.insert(0,'acc_each_class')
-        if acc_each_dataset_list!= None:
-            acc_each_dataset_list.insert(0,'acc_each_dataset')
+        best_accuracy_list.insert(0, 'best acc')
+        final_accuracy_list.insert(0, 'final acc')
+        best_epoch_list.insert(0, 'best epoch')
+        avg_valid_acc_50_list.insert(0, 'avg_valid_acc_50')
+        acc_each_class_list.insert(0, 'acc_each_class')
+        if acc_each_dataset_list != None:
+            acc_each_dataset_list.insert(0, 'acc_each_dataset')
             writer.writerow(acc_each_dataset_list)
         writer.writerow(best_accuracy_list)
         writer.writerow(final_accuracy_list)
         writer.writerow(best_epoch_list)
-        writer.writerow(ave_valid_acc_50_list)
+        writer.writerow(avg_valid_acc_50_list)
         writer.writerow(acc_each_class_list)
         self.csv_file.flush()
 
